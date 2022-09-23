41a5bf3f28c6d215e4546047e8daf87cec02f4ea
just-hope-netizen <hopepeter007@gmail.com>
2022-09-23 16:12:05 +0100

    Navbar completed

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,31 +1,37 @@
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import { Logo, Chevron } from "../../assets/svgs";
-import SearchBar from "../Search";
-import ThemeSwitch from "../ThemeSwitch";
-import { StarIcon } from "../../assets/svgs";
-import styles from "./navbar.module.scss";
+import { Chevron, Logo, StarIcon } from '../../assets/svg';
+import Menu from '../../layout/Menu';
+import SearchBar from '../Search';
+import styles from './navbar.module.scss';
 
 const Navbar = () => {
+  const [menu, setMenu] = useState();
+
+  const toggleMenu = () => {
+    setMenu(!menu);
+  };
   return (
     <nav className={styles.nav}>
       <Logo />
-      <div className={styles.nav__links}>
-        <Link to="/">
-          Personal <Chevron />
-        </Link>
-        <Link to="/business">
-          Business <Chevron />
-        </Link>
-        <SearchBar />
-      </div>
-      <div className={styles.nav__btns}>
-        <ThemeSwitch />
-
-        <button className={styles.nav__btns__login}>Login</button>
-        <button className={styles.nav__btns__started}>
-          Get started <StarIcon />
-        </button>
+      <Menu onClick={toggleMenu} className={styles.menu}/>
+      <div className={`${styles.links__wrapper } ${menu && styles.links__wrapper__active}`}>
+        <div className={styles.nav__links}>
+          <Link to='/'>
+            Personal <Chevron />
+          </Link>
+          <Link to='/business'>
+            Business <Chevron />
+          </Link>
+          <SearchBar />
+        </div>
+        <div className={styles.nav__btns}>
+          <button className={styles.nav__btns__login}>Login</button>
+          <button className={styles.nav__btns__started}>
+            Get started <StarIcon />
+          </button>
+        </div>
       </div>
     </nav>
   );
